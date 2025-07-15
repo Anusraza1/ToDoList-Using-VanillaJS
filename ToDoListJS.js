eb9ab5bb4e48d0af153d11a9c3411cd006ee2354
Anusraza1 <ranus1111@gmail.com>
2025-07-15 18:52:43 +0500

    html and css is almost complete

---
+++ ToDoListJS.js
@@ -0,0 +1,11 @@
+const task_input = document.getElementById('task-input')
+const add_btn = document.getElementById('add-btn')
+const task_list = document.getElementById('task-list')
+
+let tasks=[]
+
+function getTask() {
+    task_input.textContent = task[1]
+}
+
+
